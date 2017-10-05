5030f9e474a09324cf902335e659127a8eeba2e8
tsopunish <32548740+tsopunish@users.noreply.github.com>
2017-10-05 21:56:37 +0300

    Add files via upload

---
+++ Standar Cca + offset/offset.m
@@ -0,0 +1,12 @@
+function er = offset()
+
+a=rand();
+    
+    if a<0.5
+        er=-rand();
+    else
+        er=rand();
+    end
+
+end
+    
---
+++ Standar Cca + offset/printroom.m
@@ -0,0 +1,20 @@
+function M = printroom( room )
+    cm = [1,1,1; 0,0,0; 0,1,0; 1,0,0; 0,0,1];
+        
+    M=zeros(21,33);
+    k=3;
+    l=3;
+    for i=1:9
+        for j=1:15     
+            M(k,l)=room(i,j);
+            l=l+2;
+        end
+        l=3;
+        k=k+2;
+    end
+    
+    imshow(M,cm,'InitialMagnification','fit');
+    pause(0.02);
+
+end
+
